fcd018dd21c9ce5ee1c48252f9ed6ae590a170b7
mistersuun <58406917+mistersuun@users.noreply.github.com>
2023-10-31 21:48:32 +0100

    cleanup (#19)
    * change css and footer
    
    * delete favicon
    
    * portfolio page update with redirect
    
    * update sidebanner and portfolio
    
    ---------
    
    Co-authored-by: mac <mac@macs-MacBook-Pro.local>

--- arvilax/src/components/common/Menu/OffCanvas.js
+++ arvilax/src/components/common/Menu/OffCanvas.js
@@ -10,7 +10,7 @@ const OffCanvas = ({ setOffCanvas, isOffCanvas }) => {
         className={`offcanvas__area off-canvas-bg ${
           isOffCanvas ? "opened" : ""
         }`}
-        style={{ backgroundImage: `url("assets/img//bg/Sidearea-bg-img.png")` }}
+        /* style={{ backgroundImage: `url("assets/img//bg/Sidearea-bg-img.png")` }}*/
       >
         <div className="offcanvas_area-logo">
           <span className="offcanvas__close-btn">
@@ -75,27 +75,18 @@ const OffCanvas = ({ setOffCanvas, isOffCanvas }) => {
           </div>
         </div>
 
+      {/*
         <div className="tp-footer__widget pb-30 offcanvas-ct-info">
           <h3 className="tp-footer__widget-title">Quick Links</h3>
           <ul>
             <li>
-              <Link to="#">27 Division St, New York, NY 10002, USA</Link>
-            </li>
-            <li>
-              <Link to="tel:+88015569569365">(+880)52462545632</Link>
-            </li>
-            <li>
               <Link to="mailto:support@example.com">support@example.com</Link>
-            </li>
-            <li>
-              <span> Office Hours: 9AM - 4PM</span>
-            </li>
-            <li>
-              <span> Friday - Wekend Day</span>
             </li>
           </ul>
         </div>
+      */}
       </div>
+      
       <div
         onClick={() => setOffCanvas(false)}
         className={`body-overlay ${isOffCanvas ? "opened" : ""}`}
