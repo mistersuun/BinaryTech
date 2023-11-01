9d9761ad05dd853eda95d8e870ef05e9fa9cb1ea
mac <mac@macs-MacBook-Pro.local>
2023-11-01 13:04:14 +0100

    video modified again

--- arvilax/src/components/Pages/About/AboutFeature.js
+++ arvilax/src/components/Pages/About/AboutFeature.js
@@ -74,7 +74,7 @@ const AboutFeature = () => {
       <VideoPopup
         isVideoOpen={isVideoOpen}
         setIsVideoOpen={setIsVideoOpen}
-        videoId={"3exrJ9_Ze_o"}
+        videoId={"0gTKlztcXU0"}
       />
       {/* video modal end */}
     </>
