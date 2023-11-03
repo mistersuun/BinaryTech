72f269603f2b709e94853aa52182631bb39996d0
mac <mac@macs-MacBook-Pro.local>
2023-11-03 17:11:30 +0100

    update menu data

--- arvilax/src/components/common/Menu/MobileMenu.js
+++ arvilax/src/components/common/Menu/MobileMenu.js
@@ -17,54 +17,9 @@ const MobileMenu = () => {
               <li>
                 <Link to="/service">Services</Link>
               </li>
-              {/*
-              <li className="has-dropdown">
-                <Link to="/team">Pages</Link>
-                <ul className="submenu">
-                  
-                  <li>
-                    <Link to="/team">Team</Link>
-                  </li>
-                  <li>
-                    <Link to="/team-details">Team details</Link>
-                  </li>
-                  <li>
-                    <Link to="/service-details">service details</Link>
-                  </li>
-                  <li>
-                    <Link to="/price">Price</Link>
-                  </li>
-                  <li>
-                    <Link to="/contact">contact</Link>
-                  </li>
-                </ul>
-              </li>
-              */}
               <li>
                 <Link to="/portfolio">Portfolio</Link>
-              {/* 
-                <ul className="submenu">
-                  <li>
-                    <Link to="/portfolio">Portfolio</Link>
-                  </li>
-                  <li>
-                    <Link to="/portfolio-details">Portfolio Details</Link>
-                  </li>
-                </ul>
-              */}
               </li>
-          {/* <li className="has-dropdown">
-                <Link to="/blog">Blog</Link>
-                <ul className="submenu">
-                  <li>
-                    <Link to="/blog">blog</Link>
-                  </li>
-                  <li>
-                    <Link to="/blog-details">blog Details</Link>
-                  </li>
-                </ul>
-              </li>
-            */}
             <li>
               <Link to="/faq">Faq</Link>
             </li>
